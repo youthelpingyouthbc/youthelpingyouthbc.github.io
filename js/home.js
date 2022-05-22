ded1fe4db43648716d6c1f89f3809fb4aee8ae7f
DonaldKLee <Donald3010@outlook.com>
2022-05-21 17:43:17 -0700

    Worked on impact section

--- js/home.js
+++ js/home.js
@@ -1,38 +1,31 @@
 function increase_impact_counter() {
     impact_1_text = document.getElementById("impact_1").innerHTML;
     impact_1_final = 40000;
-
     if (parseInt(impact_1_text) !== parseInt(impact_1_final)) {
         document.getElementById("impact_1").innerHTML = parseInt(impact_1_text) + (impact_1_final*0.01);
     }
 
     impact_2_text = document.getElementById("impact_2").innerHTML;
     impact_2_final = 500;
-
     if (parseInt(impact_2_text) !== parseInt(impact_2_final)) {
         document.getElementById("impact_2").innerHTML = parseInt(impact_2_text) + (impact_2_final*0.01);
     }
 
-
     impact_3_text = document.getElementById("impact_3").innerHTML;
     impact_3_final = 20000;
-
     if (parseInt(impact_3_text) !== parseInt(impact_3_final)) {
         document.getElementById("impact_3").innerHTML = parseInt(impact_3_text) + (impact_3_final*0.01);
     }
 
     impact_4_text = document.getElementById("impact_4").innerHTML;
     impact_4_final = 40000;
-
     if (parseInt(impact_4_text) !== parseInt(impact_4_final)) {
         document.getElementById("impact_4").innerHTML = parseInt(impact_4_text) + (impact_4_final*0.01);
     }
 }
 
-
-
 function impact_counter() {
-    setInterval(increase_impact_counter, 5);
+    setInterval(increase_impact_counter, 10);
 }
 
 impact_counter()
