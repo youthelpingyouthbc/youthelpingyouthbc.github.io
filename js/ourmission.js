bfee01b85d8381a23f9117c69d1358b438a959a6
Kelly Hum <kellyhum88@gmail.com>
2022-07-09 15:58:19 -0700

    create new js file for our mission page

---
+++ js/ourmission.js
@@ -0,0 +1,11 @@
+const teamCircles = () => {
+    let yhyLogo = document.querySelector('#team-yhylogo');
+    let headShot = document.querySelectorAll('.small-circle');
+    let logoOffset = 2;
+
+    for (const i in headShot) {
+       
+    }
+}
+
+teamCircles()
