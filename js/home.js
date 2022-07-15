762c39bfd7aef127ce3073402a6f2aa4221d66fb
DonaldKLee <Donald3010@outlook.com>
2022-07-15 15:26:08 -0700

    Completed home page

--- js/home.js
+++ js/home.js
@@ -1,24 +1,24 @@
 function increase_impact_counter() {
     impact_1_text = document.getElementById("impact_1").innerHTML;
-    impact_1_final = 40000;
+    impact_1_final = 500000;
     if (parseInt(impact_1_text) !== parseInt(impact_1_final)) {
         document.getElementById("impact_1").innerHTML = parseInt(impact_1_text) + (impact_1_final*0.01);
     }
 
     impact_2_text = document.getElementById("impact_2").innerHTML;
-    impact_2_final = 500;
+    impact_2_final = 100;
     if (parseInt(impact_2_text) !== parseInt(impact_2_final)) {
         document.getElementById("impact_2").innerHTML = parseInt(impact_2_text) + (impact_2_final*0.01);
     }
 
     impact_3_text = document.getElementById("impact_3").innerHTML;
-    impact_3_final = 20000;
+    impact_3_final = 10;
     if (parseInt(impact_3_text) !== parseInt(impact_3_final)) {
-        document.getElementById("impact_3").innerHTML = parseInt(impact_3_text) + (impact_3_final*0.01);
+        document.getElementById("impact_3").innerHTML = parseInt(impact_3_text) + (impact_3_final*0.1);
     }
 
     impact_4_text = document.getElementById("impact_4").innerHTML;
-    impact_4_final = 40000;
+    impact_4_final = 4000;
     if (parseInt(impact_4_text) !== parseInt(impact_4_final)) {
         document.getElementById("impact_4").innerHTML = parseInt(impact_4_text) + (impact_4_final*0.01);
     }
@@ -30,7 +30,7 @@ function impact_counter() {
 
 
 window.onscroll = function(event) {
-    impact_section_y = document.getElementById("impact_containers").getBoundingClientRect().top - document.body.getBoundingClientRect().top - 400
+    impact_section_y = document.getElementById("impact_containers").getBoundingClientRect().top - document.body.getBoundingClientRect().top - 500
 
     if (impact_section_y <= scrollY) {
         impact_counter()
